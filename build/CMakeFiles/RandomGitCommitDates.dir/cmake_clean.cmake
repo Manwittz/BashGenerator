cb880fafb6f3284cfa5100a7cc6053b120f6d97a
[MASTER] roshii <Roshii@stibits.io>
2023-10-03 15:08:50 -0400

    gitingore

---
+++ build/CMakeFiles/RandomGitCommitDates.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RandomGitCommitDates.dir/src/main.cpp.o"
+  "CMakeFiles/RandomGitCommitDates.dir/src/main.cpp.o.d"
+  "RandomGitCommitDates"
+  "RandomGitCommitDates.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/RandomGitCommitDates.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
